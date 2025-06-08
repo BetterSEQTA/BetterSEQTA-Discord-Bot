d0b125e753a3914d50d20e8d9513c6dda7a98c25
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-06-08 18:52:54 +0800

    feat: logging for now

--- src/events/bug-reports.ts
+++ src/events/bug-reports.ts
@@ -3,6 +3,6 @@ import { AnyThreadChannel, Events } from "discord.js";
 export default {
     name: Events.ThreadCreate,
     async execute(thread: AnyThreadChannel, newlyCreated: boolean) {
-        
+        console.log(thread, newlyCreated);
     }
 }
