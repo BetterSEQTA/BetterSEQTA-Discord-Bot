0df60aac9bb88d50bceeb1855db1d1e41361069c
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-06-08 18:58:42 +0800

    feat: start of bugreports

--- src/events/bug-reports.ts
+++ src/events/bug-reports.ts
@@ -1,8 +1,8 @@
 import { AnyThreadChannel, Events } from "discord.js";
 
 export default {
-    name: Events.ThreadCreate,
-    async execute(thread: AnyThreadChannel, newlyCreated: boolean) {
-        console.log(thread, newlyCreated);
+    name: Events.ThreadUpdate,
+    async execute(oldThread: AnyThreadChannel, newThread: AnyThreadChannel) {
+        console.log(oldThread, newThread)
     }
 }
