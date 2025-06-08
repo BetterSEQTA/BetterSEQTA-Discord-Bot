1b5d3315baab49bcb0cc1d18d8fcfff251c4ee47
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-06-08 20:22:11 +0800

    only in a specific channel

--- src/events/githubUpdates.ts
+++ src/events/githubUpdates.ts
@@ -7,7 +7,7 @@ export default {
     async execute(message: OmitPartialGroupDMChannel<Message>) {
         if (message.webhookId) {
             pollChanges(message);
-        } else if (message.mentions.has(message.client.user!) && !message.author.bot) {
+        } else if (message.mentions.has(message.client.user!) && !message.author.bot && message.channelId == "1381245393245044847") {
             const tenmessages = await message.channel.messages.fetch({ limit: 10})
             const response = await talkToGemini(message.content, tenmessages, message.author!);
             if (response !== '') {
