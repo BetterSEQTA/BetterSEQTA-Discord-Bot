818e5729496cad8e1bb5264267700aa955695437
CyberHydra <164726019+Hydra777777@users.noreply.github.com>
2025-09-14 14:49:04 +1000

    feat: added actual bot latency in ms

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -4,8 +4,10 @@ import { ChatInputCommandInteraction } from 'discord.js';
 export default {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Check bot latency!'),
     async execute(interaction: ChatInputCommandInteraction) {
-        await interaction.reply('Pong!');
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        const latency = sent.createdTimestamp - interaction.createdTimestamp;
+        await interaction.editReply(`Pong! (${latency}ms)`);
     },
 }
